e05eee546c8f12c944e6eb64fd0f9c36a70a835d
William Makley <info@willmakley.dev>
2019-11-10 22:13:53 -0500

    remove dead code

--- src/Shaders.js
+++ src/Shaders.js
@@ -1,4 +1,3 @@
 // Bridge between shaders that are plain text files and ES 6 modules
 export const VertexPassThrough = require('./shaders/VertexPassThrough.glsl');
 export const Fragment = require ('./shaders/Fragment.glsl');
-export const FragmentTest = require('./shaders/FragmentTest.glsl');
